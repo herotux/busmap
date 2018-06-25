737e31a3aba9604e88f4e67ceb3c8ae53fae09a7
freetux <gshs99@gmail.com>
2018-06-26 01:18:28 +0430

    lastest changes

---
+++ dist/kshmap.js
@@ -0,0 +1,581 @@
+var kshbus = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2018-06-25T13:16:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2642437540",
+        "highway": "bus_stop",
+        "name": "ایستگاه فرهنگیان",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.1402739,
+          34.3614682
+        ]
+      },
+      "id": "node/2642437540"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2653505727",
+        "highway": "bus_stop",
+        "name": "ایستگاه ۱ فرهنگیان",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.1166141,
+          34.3628087
+        ]
+      },
+      "id": "node/2653505727"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/2653505728",
+        "highway": "bus_stop",
+        "name": "ایستگاه ۲ فرهنگیان",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.1190403,
+          34.3614962
+        ]
+      },
+      "id": "node/2653505728"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3029508315",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0436802,
+          34.3036765
+        ]
+      },
+      "id": "node/3029508315"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3029512698",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0481661,
+          34.3077201
+        ]
+      },
+      "id": "node/3029512698"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3029512699",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0529334,
+          34.3125711
+        ]
+      },
+      "id": "node/3029512699"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3029512700",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0575327,
+          34.3183414
+        ]
+      },
+      "id": "node/3029512700"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4394350408",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0730343,
+          34.3225601
+        ]
+      },
+      "id": "node/4394350408"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4458758505",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0749641,
+          34.3233867
+        ]
+      },
+      "id": "node/4458758505"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4471020267",
+        "amenity": "bus_station",
+        "highway": "bus_stop",
+        "name": "ایستگاه اتوبوس",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.1048641,
+          34.3826044
+        ]
+      },
+      "id": "node/4471020267"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4478426494",
+        "highway": "bus_stop",
+        "name": "ایستگاه اتوبوس",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.100436,
+          34.3447032
+        ]
+      },
+      "id": "node/4478426494"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4478428550",
+        "highway": "bus_stop",
+        "name": "ایستگاه اتوبوس",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0957317,
+          34.3404987
+        ]
+      },
+      "id": "node/4478428550"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4478431920",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0892666,
+          34.3360509
+        ]
+      },
+      "id": "node/4478431920"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4478494813",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0713795,
+          34.3190019
+        ]
+      },
+      "id": "node/4478494813"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4484993426",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0826798,
+          34.3309764
+        ]
+      },
+      "id": "node/4484993426"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4485018692",
+        "highway": "bus_stop",
+        "name": "ایستگاه اتوبوس",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.097521,
+          34.3407711
+        ]
+      },
+      "id": "node/4485018692"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4486585193",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0893177,
+          34.3355649
+        ]
+      },
+      "id": "node/4486585193"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4486585196",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0935626,
+          34.3385078
+        ]
+      },
+      "id": "node/4486585196"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4548760104",
+        "highway": "bus_stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.1035353,
+          34.3475797
+        ]
+      },
+      "id": "node/4548760104"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4936370572",
+        "highway": "bus_stop",
+        "name": "ایستگاه اتوبوس",
+        "name:en": "bus stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.1730278,
+          34.3577611
+        ]
+      },
+      "id": "node/4936370572"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4936376628",
+        "highway": "bus_stop",
+        "name": "ایستگاه اتوبوس",
+        "name:en": "bus stop",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.1672238,
+          34.3577007
+        ]
+      },
+      "id": "node/4936376628"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5264221777",
+        "highway": "bus_stop",
+        "name": "امیری",
+        "name:en": "amiri",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0730725,
+          34.3221131
+        ]
+      },
+      "id": "node/5264221777"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5264221778",
+        "highway": "bus_stop",
+        "name": "پارسیان",
+        "name:en": "parsian",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0693024,
+          34.3151337
+        ]
+      },
+      "id": "node/5264221778"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5264221779",
+        "highway": "bus_stop",
+        "name": "چهارراه اجاق",
+        "name:en": "ojaq",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0673144,
+          34.3124614
+        ]
+      },
+      "id": "node/5264221779"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5264221780",
+        "highway": "bus_stop",
+        "name": "آل آقا",
+        "name:en": "al aqa",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0709836,
+          34.3175626
+        ]
+      },
+      "id": "node/5264221780"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5264221783",
+        "highway": "bus_stop",
+        "name": "میدان انقلاب",
+        "name:en": "enghelab",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0647056,
+          34.3087825
+        ]
+      },
+      "id": "node/5264221783"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5691104961",
+        "bench": "yes",
+        "highway": "bus_stop",
+        "shelter": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0681391,
+          34.313937
+        ]
+      },
+      "id": "node/5691104961"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5694058353",
+        "bench": "no",
+        "bus": "yes",
+        "highway": "bus_stop",
+        "name": "شاهد",
+        "name:en": "shahed",
+        "public_transport": "bus"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0728447,
+          34.3587427
+        ]
+      },
+      "id": "node/5694058353"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5694644729",
+        "highway": "bus_stop",
+        "name": "میدان فردوسی",
+        "name:en": "Ferdowsi Square"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0549262,
+          34.2935613
+        ]
+      },
+      "id": "node/5694644729"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5696138321",
+        "bench": "yes",
+        "highway": "bus_stop",
+        "name": "منصف",
+        "public_transport": "platform",
+        "shelter": "yes",
+        "wheelchair": "no"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0581325,
+          34.3009396
+        ]
+      },
+      "id": "node/5696138321"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5715173070",
+        "bus": "yes",
+        "highway": "bus_stop",
+        "name": "اشرفی اصفهانی",
+        "name:en": "ashrafi isfahani",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.060658,
+          34.3031793
+        ]
+      },
+      "id": "node/5715173070"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/5715173072",
+        "bus": "yes",
+        "highway": "bus_stop",
+        "name": "دبیر اعظم",
+        "name:en": "dabir azam",
+        "public_transport": "platform"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          47.0627593,
+          34.3062963
+        ]
+      },
+      "id": "node/5715173072"
+    }
+  ]
+}
+
+
+var line10 =  {"type":"FeatureCollection",
+"features":[{"geometry":
+{"type":"LineString",
+"coordinates":[
+   [47.0549262,34.2935613],
+   [47.0581325,34.3009396],
+   [47.060658,34.3031793],
+   [47.0627593,34.3062963],
+   [47.0647056,34.3087825],
+[47.0673144,34.3124614],
+[47.0693024,34.3151337],
+[47.0709836,34.3175626],
+[47.0730725,34.3221131]]},"type":"Feature",
+"properties":{"line":1,"start":"undefined,undefined",
+"end":"undefined,undefined"}}]}
+
+var myStyle = {
+  "color": "#ff7800",
+  "weight": 5,
+  "opacity": 0.65
+};
